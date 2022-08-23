ea1180fe7b98a9328a3f207afceaf1d1f5e6a464
pooja <pooja@DESKTOP-3P7GERF>
2022-08-23 17:59:10 +0530

    CRUD_operations

---
+++ DemoOnCRUD/src/com/tns/client/Client.java
@@ -0,0 +1,51 @@
+package com.tns.client;
+import com.tns.entity.Student;
+import com.tns.service.StudentService;
+import com.tns.service.StudentServiceImpl;
+
+
+public class Client 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		// CRUD operation calling activity
+		Student student = new Student();
+		
+		StudentService service = new StudentServiceImpl();
+		
+		// Create Operation
+		
+		//student.setStudentid(1);
+	//student.setName("Avantika ");
+	//service.addStudent(student);
+		
+		
+	// Retrieve Operation
+	//	service.findStudentById(104);
+		//System.out.println("ID is: "+student.getStudentid());
+		//System.out.println("Name is: "+student.getName());
+		
+		
+		
+		//Update Operation
+		student  =service.findStudentById(1);
+		student.setName("Avantika ");
+		service.updateStudent(student);
+		
+		
+	/*	service.findStudentById(101);
+		System.out.println("ID is: "+student.getStudentid());
+		System.out.println("Name is: "+student.getName());
+		
+		
+		// Delete Operation
+		student=service.findStudentById(104);
+		service.removeStudent(student);
+		System.out.println("Row removed");*/
+		System.out.println("End of program/Life Cycle is Completed...");
+		
+	}
+
+}
